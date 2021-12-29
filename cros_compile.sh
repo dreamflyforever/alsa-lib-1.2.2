fc217e1f6aadbcd26473acbdf16d8dc9459d7a1b
jim <1150660744@qq.com>
2021-12-29 11:16:24 +0800

    add cross_compile.sh
    Signed-off-by: jim <1150660744@qq.com>

---
+++ cros_compile.sh
@@ -0,0 +1,9 @@
+### libasound2-dev
+source code of a libasound2-dev: apt-get source libasound2-dev
+https://salsa.debian.org/alsa-team/alsa-lib
+
+### corss compile
+make distclean; ./configure --host=aarch64-linux-gnu ; make 
+
+### license
+MIT by Jim
